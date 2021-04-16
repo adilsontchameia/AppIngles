3a2190b84d93be019708812c4b6da91508bb3d6c
adilsonchameia <adilsonkchameia@outlook.com>
2021-04-16 20:45:48 +0100

    classes de navegacao das tabs

---
+++ lib/telas/Animais.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Animais extends StatefulWidget {
+  @override
+  _AnimaisState createState() => _AnimaisState();
+}
+
+class _AnimaisState extends State<Animais> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
